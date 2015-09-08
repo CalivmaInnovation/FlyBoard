ac85096c494290100536b5e95cd90b705850c933
Alejandro Almira <aalmiramolla@gmail.com>
2015-09-08 13:18:33 +0200

    Trying

--- cfg/build_config.mk
+++ cfg/build_config.mk
@@ -48,7 +48,7 @@
 
 # Name of the project (without spaces, as it will be used as filename)
 #   and Z80 memory location where code will start in the generated binary
-PROJNAME   := CPCRetroDev Game
+PROJNAME   := CPCRetroDevGame
 Z80CODELOC := 0x4000
 
 # Folders 
