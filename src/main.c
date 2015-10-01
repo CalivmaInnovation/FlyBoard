48dca79aa1eb6ae834993ae3d007472e026fc141
Cesar_Snake <snakemgs14@gmail.com>
2015-10-01 21:58:58 +0000

    Refactorizacion de codigo

--- src/main.c
+++ src/main.c
@@ -26,7 +26,7 @@
 #define MODE0_HEIGHT 190
 #define MODE0_WIDTH 75
 
-const u8 g_palette[7] = { 0, 26, 11, 22, 13, 6, 2, 8 };
+const u8 g_palette[8] = { 0, 26, 11, 22, 13, 6, 2, 8 };
 
 void drawMap() {
 	// Set the internal tileset for drawing Tilemaps
@@ -38,8 +38,8 @@ void drawMap() {
 
 void init() {
 	cpct_disableFirmware();
-	cpct_fw2hw(g_palette,7);
-	cpct_setPalette(g_palette,7);
+	cpct_fw2hw(g_palette,8);
+	cpct_setPalette(g_palette,8);
 	cpct_setBorder (g_palette[1]);
 	cpct_setVideoMode(0);
 
