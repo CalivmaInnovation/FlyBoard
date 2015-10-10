c2c471b5fad83de165089d6133f203e83657459f
Cesar_Snake <snakemgs14@gmail.com>
2015-10-10 17:43:22 +0200

    Car Runner Sprite

--- src/scroll.c
+++ src/scroll.c
@@ -4,17 +4,6 @@
 i8 cx, cy;
 ///////////////////////
 
-const u8 bloque [32] = {
-	0x2F, 0x0F, 0x0F, 0x4F,
-	0x1F, 0x0F, 0x0F, 0x8A,
-	0x0F, 0xCF, 0xCF, 0x00,
-	0x0F, 0xCF, 0xCF, 0x00,
-	0x0F, 0xCF, 0xCF,	0x00,
-	0x0F, 0xCF, 0xCF, 0x00,
-	0x4F, 0x00, 0x00, 0x8A,
-	0x8A, 0x00, 0x00, 0x45
-
-};
 void initScroll() {
   cx=0;
   cy=0;
@@ -30,6 +19,21 @@ void widthImageScroll(u8 tile[], u8 shadowTile[], i8 posY, u8 widthImage) {
 				scroll(tile, shadowTile, i, posY,-1);
 			}
 		}
+}
+
+void scroll(u8 tile[], u8 shadowTile[], i8 posX, i8 posY, i8 nextLine) {
+	i8 position;
+	position = (posX+cx) % 20;
+	cpct_drawSprite(tile, (u8*)0xc04c-(4*position) + 0x0050*posY, 4, 8);
+
+	if (position==0) {
+		cpct_drawSprite(shadowTile, (u8*)0xc000 + 0x50*posY, 4, 8);
+	} else {
+		cpct_drawSprite(shadowTile, (u8*)0xc04c-(4*position)+4 + 0x50*posY, 4, 8);
+	}
+
+	if (nextLine>0) { ++cx; }
+	if (cx>20) { cx=0; }
 }
 
 void Road() {
@@ -71,17 +75,6 @@ void Road() {
 	scroll(g_tile_white, g_tile_pastel_blue, i, posY,1);
 }
 
-void scroll(u8 tile[], u8 shadowTile[], i8 posX, i8 posY, i8 nextLine) {
-	i8 position;
-	position = (posX+cx) % 20;
-	cpct_drawSprite(tile, (u8*)0xc04c-(4*position) + 0x0050*posY, 4, 8);
+void CarRunner() {
 
-	if (position==0) {
-		cpct_drawSprite(shadowTile, (u8*)0xc000 + 0x50*posY, 4, 8);
-	} else {
-		cpct_drawSprite(shadowTile, (u8*)0xc04c-(4*position)+4 + 0x50*posY, 4, 8);
-	}
-
-	if (nextLine>0) { ++cx; }
-	if (cx>20) { cx=0; }
 }
--- src/scroll.h
+++ src/scroll.h
@@ -5,8 +5,8 @@
 
 void initScroll();
 void widthImageScroll(u8 tile[], u8 shadowTile[], i8 posY, u8 widthImage);
-void Road();
 void scroll(u8 tile[], u8 shadowTile[], i8 posX, i8 posY, i8 nextLine);
-
+void Road();
+void CarRunner();
 
 #endif /* end of include guard: _SCROLL_H_ */
