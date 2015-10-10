dce384c3ca9d8e1082e83b70ee932a993ee61f4d
Alejandro Almira <aalmiramolla@gmail.com>
2015-10-10 21:44:58 +0200

    end palette

--- src/tiles.h
+++ src/tiles.h
@@ -4,24 +4,19 @@
 
 #include <types.h>
 
+// Background tilemap created with tiled.
+extern const u8 g_background[40*50];
 extern u8* const g_tile_tileset_bg[3];
 
 extern const u8 g_tile_tiles_bg_0[8];
 extern const u8 g_tile_tiles_bg_1[8];
 extern const u8 g_tile_tiles_bg_2[8];
 
-//extern u8* const g_tile_tileset_bg[3];
-extern u8* const g_tile_tileset[16];
-
-extern const u8 g_tile_black_bg[8];
-extern const u8 g_tile_sky_blue_bg[8];
-extern const u8 g_tile_gray_bg[8];
 
 extern const u8 g_tile_black[32];
-extern const u8 g_tile_blue[32];
 extern const u8 g_tile_bright_blue[32];
 extern const u8 g_tile_red[32];
-extern const u8 g_tile_magenta[32];
+extern const u8 g_tile_mauve[32];
 extern const u8 g_tile_bright_red[32];
 extern const u8 g_tile_bright_magenta[32];
 extern const u8 g_tile_green[32];
@@ -30,37 +25,12 @@ extern const u8 g_tile_gray[32];
 extern const u8 g_tile_pastel_blue[32];
 extern const u8 g_tile_orange[32];
 extern const u8 g_tile_pink[32];
-extern const u8 g_tile_pastel_magenta[32];
+extern const u8 g_tile_bright_cyan[32];
+extern const u8 g_tile_pastel_green[32];
 extern const u8 g_tile_bright_yellow[32];
 extern const u8 g_tile_white[32];
 
-// Player sprites
-extern const u8 sprite_skate[32];
 extern const u8 sprite_character[384];
-
-// Runner left sprites
+extern const u8 sprite_skate[32];
 extern const u8 sprite_car[32];
-
-// Backgroung
-extern const u8 g_background[40*50];
-
-// Runners:
-
-// Car Runner
-extern const u8 g_tile_carRunner_0[32];
-extern const u8 g_tile_carRunner_1[32];
-extern const u8 g_tile_carRunner_2[32];
-extern const u8 g_tile_carRunner_3[32];
-extern const u8 g_tile_carRunner_4[32];
-extern const u8 g_tile_carRunner_5[32];
-extern const u8 g_tile_carRunner_6[32];
-extern const u8 g_tile_carRunner_7[32];
-extern const u8 g_tile_carRunner_8[32];
-extern const u8 g_tile_carRunner_9[32];
-extern const u8 g_tile_carRunner_10[32];
-extern const u8 g_tile_carRunner_11[32];
-extern const u8 g_tile_carRunner_12[32];
-extern const u8 g_tile_carRunner_13[32];
-extern const u8 g_tile_carRunner_14[32];
-
 #endif
