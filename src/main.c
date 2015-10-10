c2f94447e5432086948b672caf841f7cbe4378ad
Alejandro Almira <aalmiramolla@gmail.com>
2015-10-10 22:00:55 +0200

    Changed car left sprite

--- src/main.c
+++ src/main.c
@@ -54,9 +54,9 @@ void main(void) {
 	init();
 	// Loop forever
 	while (1) {
-		player();
 		Road();
 		Building();
+		player();
 		// Synchronize with VSYNC + 1 HSYNC to slow down the movement
 		cpct_waitVSYNC();   // Wait for VSYNC signal
 		__asm__("halt");    // H
