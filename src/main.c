8c2df186c245c02757ff6267019dd179f4fe9f7e
Alejandro Almira <aalmiramolla@gmail.com>
2015-10-12 13:27:22 +0200

    Fixed sprites backgroung and strings

--- src/main.c
+++ src/main.c
@@ -35,20 +35,18 @@ void init_game() {
 }
 
 void main(void) {
-	 u8 i=0;
-
 	// Clear Screen
 	cpct_memset(SCR_VMEM, 0, 0x4000);
 	init_CPC();
 	init_game();
 	// Loop forever
 	while (1) {
+		waitNVSYNCs(1);
 		computeWorld();
 		player();
 		createRunnerCar(0);
 		drawPlayer();
 		// Synchronize with VSYNC + 1 HSYNC to slow down the movement
-		cpct_waitVSYNC();   // Wait for VSYNC signal
-		__asm__("halt");    // H
+		waitNVSYNCs(2);
 	};
 }
